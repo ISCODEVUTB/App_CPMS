582ac81a921b64a1721f0c5f42755ed5c9a90a0c
dMuvdi <98854794+dMuvdi@users.noreply.github.com>
2022-11-21 22:24:36 -0500

    updated import in serializer

--- auth_ms/msAuthApp/views/UserCreateAPIView.py
+++ auth_ms/msAuthApp/views/UserCreateAPIView.py
@@ -5,7 +5,7 @@ from rest_framework import generics, response, status
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 # Imported user serializer
-from ..serializers.UserSerializer import UserSerializer
+from ..serializers import UserSerializer
 
 
 # CreateAPIView (method: POST)
