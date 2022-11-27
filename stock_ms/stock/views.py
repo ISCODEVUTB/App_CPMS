f1eaa9d28d03dfd2a61e1501417d4e8466746de3
dMuvdi <98854794+dMuvdi@users.noreply.github.com>
2022-11-27 17:29:07 -0500

    main views updated

--- stock_ms/stock/views.py
+++ stock_ms/stock/views.py
@@ -1,5 +1,6 @@
 from django.http.response import JsonResponse
+from django.views.decorators.http import require_safe
 
-
+@require_safe
 def home(request):
     return JsonResponse({"server": "running successfully"}, safe=False)
