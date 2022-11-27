c807e58b811ff7810e1ee39dcf3126286b8c664f
dMuvdi <98854794+dMuvdi@users.noreply.github.com>
2022-11-27 16:51:00 -0500

    updated settings

--- auth_ms/msAuthProject/settings.py
+++ auth_ms/msAuthProject/settings.py
@@ -13,13 +13,6 @@ import django_heroku
 from os import getenv
 from datetime import timedelta
 from pathlib import Path
-from django.conf import settings
-
-settings.configure(DEBUG=True)  # Sensitive when set to True
-settings.configure(DEBUG_PROPAGATE_EXCEPTIONS=True)  # Sensitive when set to True
-
-def custom_config(config):
-    settings.configure(default_settings=config, DEBUG=True)  # Sensitive
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
