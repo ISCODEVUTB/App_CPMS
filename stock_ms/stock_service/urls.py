85c1e9bb60260cd8b3c9c1448e1d968cdf72b3dc
dMuvdi <98854794+dMuvdi@users.noreply.github.com>
2022-11-29 23:44:19 -0500

    modified urls

--- stock_ms/stock_service/urls.py
+++ stock_ms/stock_service/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_api),
-    path('<int:id>', views.product_api),
-    path('popular', views.popular),
-    path('active', views.active),
-    path('active/<int:id>', views.active),
-    path('addFromProvider', views.add_product_from_provider),
-    path('search', views.search_product),
-    path('type', views.product_by_type)
+    path('', views.product_api, name="home"),
+    path('<int:id>', views.product_api, name="product_individual"),
+    path('popular', views.popular, name="product_popular"),
+    path('active', views.active, name="product_active"),
+    path('active/<int:id>', views.active, name="product_active_by_id"),
+    path('addFromProvider', views.add_product_from_provider, name="add_from_provider"),
+    path('search', views.search_product, name="product_search"),
+    path('type', views.product_by_type, name="product_by_type")
 ]
